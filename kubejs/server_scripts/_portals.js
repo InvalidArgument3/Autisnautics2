e54d721066fc53cad010568d4685e0c38ced1cfb
regaccio <e@mail.address>
2025-09-04 13:34:50 -0400

    Breaking wasteland portals

--- kubejs/server_scripts/_portals.js
+++ kubejs/server_scripts/_portals.js
@@ -2,6 +2,7 @@
 let frameBlock = "kubejs:lead_casing"
 let rodBlock = "alexscaves:uranium_rod"
 
+//building
 BlockEvents.placed(event => {
     let placedBlock = event.getBlock()
     if (placedBlock.getId() == "supplementaries:globe_sepia") {
@@ -46,5 +47,32 @@ BlockEvents.placed(event => {
             placedBlock.set("minecraft:air")
             nuclearBomb.set("nuclearcraft:portal")
         }
+    }
+})
+
+//breaking
+BlockEvents.broken(event => {
+    let brokenBlock = event.getBlock()
+    if (brokenBlock.getId() == frameBlock) {
+        let portalBreaker = event.getEntity()
+        let server = event.getLevel().getServer()
+        let donutBlocks = [brokenBlock.getNorth(), brokenBlock.getSouth(), brokenBlock.getEast(), brokenBlock.getWest(), brokenBlock.getNorth().getEast(), brokenBlock.getNorth().getWest(), brokenBlock.getSouth().getEast(), brokenBlock.getSouth().getWest()]
+        donutBlocks.forEach(b => {
+            if (b.getId() == "nuclearcraft:portal") {
+                b.set("minecraft:air")
+                server.runCommandSilent(`/execute at ${portalBreaker.stringUUID} run playsound nuclearcraft:fusion_switch master @a ~ ~ ~ 1 1`)
+            }
+        })
+    }
+    if (brokenBlock.getId() == rodBlock) {
+        let portalBreaker = event.getEntity()
+        let server = event.getLevel().getServer()
+        let diagonalBlocks = [brokenBlock.getNorth().getWest().getDown(), brokenBlock.getSouth().getWest().getDown(), brokenBlock.getNorth().getEast().getDown(), brokenBlock.getSouth().getEast().getDown()]
+        diagonalBlocks.forEach(b => {
+            if (b.getId() == "nuclearcraft:portal") {
+                b.set("minecraft:air")
+                server.runCommandSilent(`/execute at ${portalBreaker.stringUUID} run playsound nuclearcraft:fusion_switch master @a ~ ~ ~ 1 1`)
+            }
+        })
     }
 })
